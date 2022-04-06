181e7037e85e379515ea5292817d075f40a47896
Federico Sardo <federico.sardo1@gmail.com>
2022-04-06 14:57:57 +0200

    implemented sticker selection

--- components/StickersWindow/StickersWindow.js
+++ components/StickersWindow/StickersWindow.js
@@ -1,16 +1,52 @@
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import styled from "styled-components";
 import { Icon } from "@iconify/react";
 import Image from "next/image";
 
+import { StickersArray } from "../../utils/stickers";
 import { UserContext } from "../../pages/inbox/browse";
 import OuterWindow from "../../components/OuterWindow/OuterWindow";
 import InnerWindow from "../../components/InnerWindow/InnerWindow";
 import PopupTitle from "../PopupTitle/PopupTitle";
+import { Button } from "../Button/Button";
 import Attention from "../../public/images/attention.svg";
+import Sticker from "../Sticker/Sticker";
 
 export default function StickersWindow() {
   const { showPopup, setShowPopup } = useContext(UserContext);
+  const [stickersList, setStickersList] = useState(StickersArray);
+  const [selectedSticker, setSelectedSticker] = useState();
+
+  /*
+
+  handleOnClickSticker handles the stickers selection 
+  
+  */
+
+  function handleOnClickSticker(stickerIndex) {
+    const updatedStickersList = stickersList.map((sticker, index) => {
+      /* 
+      if the sticker index is the same from the one called in the function
+      then we return a new object with the prop "isSelected" set to "true"
+        */
+      if (stickerIndex === index) {
+        if (sticker.isSelected === true) {
+          /* 
+          if the clicked sticker was already selected then we deselect it
+          returning an object where "isSelected" is "false"
+          */
+          setSelectedSticker(undefined);
+          return { ...sticker, isSelected: false };
+        }
+        setSelectedSticker(sticker.url);
+        return { ...sticker, isSelected: true };
+      } else {
+        return { ...sticker, isSelected: false };
+      }
+    });
+    setStickersList(updatedStickersList);
+  }
+
   return (
     <PopupWindow style={showPopup ? { display: "block" } : { display: "none" }}>
       <OuterWindow>
@@ -25,7 +61,21 @@ export default function StickersWindow() {
           />
         </PopupTitle>
         <InnerWindow>
-          <StickersContainer></StickersContainer>
+          <StickersContainer>
+            {stickersList.map((sticker, index) => {
+              return (
+                <Sticker
+                  key={index}
+                  image={sticker.url}
+                  isSelected={sticker.isSelected}
+                  onClickSticker={() => {
+                    handleOnClickSticker(index);
+                  }}
+                />
+              );
+            })}
+          </StickersContainer>
+          {selectedSticker ? <Button>Send sticker</Button> : null}
         </InnerWindow>
         <WarningWrapper>
           <Image src={Attention} width={250} height={250} priority={true} />
@@ -47,7 +97,15 @@ const PopupWindow = styled.aside`
 `;
 
 const StickersContainer = styled.aside`
-  height: 370px;
+  padding-right: 0.5rem;
+  padding-bottom: 0.5rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-content: flex-start;
+  gap: 1rem;
+  height: 330px;
+  overflow: auto;
 `;
 
 const WarningWrapper = styled.div`
---
+++ utils/stickers.js
@@ -0,0 +1,8 @@
+export const StickersArray = [
+  { url: "00-heart.svg", isSelected: false },
+  { url: "01-turtle.svg", isSelected: false },
+  { url: "02-unicorn.svg", isSelected: false },
+  { url: "03-monster.svg", isSelected: false },
+  { url: "04-icecream.svg", isSelected: false },
+  { url: "05-cooking.svg", isSelected: false },
+];
