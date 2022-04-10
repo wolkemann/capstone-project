2affeda9188e3690287fb3f0d8c16dc8bd4e8ef7
wolkemann <ohtoristudent@gmail.com>
2022-04-10 23:40:45 +0200

    loader in browse and inbox

--- pages/send.js
+++ pages/send.js
@@ -49,7 +49,6 @@ export default function Home() {
       return (
         <main>
           <Loader text="Sending Letter..." />
-          <Navigation />
         </main>
       );
     case "success":
